22e685a0c2136f76102fa3aa31dbb9dfcfae9f00
BrandonVG <54459465+BrandonVG@users.noreply.github.com>
2020-10-20 23:59:55 -0500

    Falta la opcion para insertar producto en posicion

--- main.js
+++ main.js
@@ -11,37 +11,46 @@ var btnAgregar = document.querySelector("#agregar");
 var btnBorrar = document.querySelector("#borrar");
 var btnBuscar = document.querySelector("#buscar");
 var btnListar = document.querySelector("#listar");
+var btnListarI = document.querySelector("#listarInv");
 var div = document.querySelector("#result");
 btnAgregar.addEventListener("click",()=>{
     cond = 0;
-    var objeto = {
-        "id":codigo.value,
-        "nombre":nombre.value,
-        "desc":desc.value,
-        "cant":cantidad.value,
-        "cost":costo.value
-    }
-    if (contador < 20){
-        productos.forEach(p =>{
-            if (objeto.id == p.id){
-                cond = 1;
+    let validacion = validar();
+    if (validacion == 1){
+        var objeto = {
+            "id":codigo.value,
+            "nombre":nombre.value,
+            "desc":desc.value,
+            "cant":cantidad.value,
+            "cost":costo.value
+        }
+        if (contador < 20){
+            productos.forEach(p =>{
+                if (objeto.id == p.id){
+                    cond = 1;
+                }
+            })
+            if(cond == 0){
+                productos[contador] = objeto;
+                contador++;
+                div.textContent="";
+                div.insertAdjacentHTML("beforeend","<p>Producto agregado.</p>");
             }
-        })
-        if(cond == 0){
-            productos[contador] = objeto;
-            contador++;
-            div.textContent="";
-            div.insertAdjacentHTML("beforeend","<p>Producto agregado.</p>");
+            else{
+                div.textContent="";
+                div.insertAdjacentHTML("beforeend","<p>Producto ya agregado.</p>");
+            }
         }
         else{
             div.textContent="";
-            div.insertAdjacentHTML("beforeend","<p>Producto ya agregado.</p>");
+            div.insertAdjacentHTML("beforeend","<p>Máximo de productos</p>");
         }
     }
     else{
         div.textContent="";
-        div.insertAdjacentHTML("beforeend","<p>Máximo de productos</p>");
+        div.insertAdjacentHTML("beforeend","<p>Algún campo esta vacío.</p>");
     }
+    
 })
 btnBorrar.addEventListener("click",()=>{
     index = encontrar(productos,codigo);
@@ -54,7 +63,6 @@ btnBorrar.addEventListener("click",()=>{
         delete productos[index];
         div.textContent="";
         div.insertAdjacentHTML("beforeend","<p>Producto eliminado.</p>");
-        
     }
 })
 btnBuscar.addEventListener("click",()=>{
@@ -78,16 +86,7 @@ btnBuscar.addEventListener("click",()=>{
    }
 })
 btnListar.addEventListener("click",()=>{
-    div.textContent="";
-    div.insertAdjacentHTML("beforeend",`<table id="t1" style="text-align:center">
-    <thead>
-    <th>Código</th>
-    <th>Nombre</th>
-    <th>Descripción</th>
-    <th>Cantidad</th>
-    <th>Costo</th>
-    <thead>
-    <tbody id="tabla" style="text-align:center"></tbody>`);
+    crearTabla();
     let tabla = document.querySelector("#tabla");
     productos.forEach(p =>{
         let ren = tabla.insertRow(-1);
@@ -103,6 +102,25 @@ btnListar.addEventListener("click",()=>{
         col4.textContent = p.cost;
     })
 })
+btnListarI.addEventListener("click",()=>{
+    crearTabla();
+    invertir(productos);
+    let tabla = document.querySelector("#tabla");
+    productos.forEach(p =>{
+        let ren = tabla.insertRow(-1);
+        let col = ren.insertCell(0);
+        let col1 = ren.insertCell(1);
+        let col2 = ren.insertCell(2);
+        let col3 = ren.insertCell(3);
+        let col4 = ren.insertCell(4);
+        col.textContent = p.id;
+        col1.textContent = p.nombre;
+        col2.textContent = p.desc;
+        col3.textContent = p.cant;
+        col4.textContent = p.cost;
+    })
+    invertir(productos);
+})
 function encontrar(vector,codigo){
     let index = 0;
     vector.forEach((p,i) =>{
@@ -111,4 +129,39 @@ function encontrar(vector,codigo){
         }
     })
     return index;
+}
+function crearTabla(){
+    div.textContent="";
+    div.insertAdjacentHTML("beforeend",`<table id="t1" style="text-align:center">
+    <thead>
+    <th>Código</th>
+    <th>Nombre</th>
+    <th>Descripción</th>
+    <th>Cantidad</th>
+    <th>Costo</th>
+    <thead>
+    <tbody id="tabla" style="text-align:center"></tbody>`);
+}
+function invertir(vector){
+    let temp;
+    let n = vector.length
+    for (let i = 0; i < n/2; i++){
+        temp = vector[i];
+        vector[i] = vector[n-i-1];
+        vector[n-i-1] = temp;
+    }
+    for(let j = 0;j<n;j++){
+        if (productos[j] == undefined){
+            delete productos[j]
+        }
+    }
+    return vector;
+}
+function validar(){
+    if (codigo.value == "" || nombre.value == "" || desc.value == "" || cantidad.value =="" || costo.value ==""){
+        return 0;
+    }
+    else{
+        return 1;
+    }
 }
