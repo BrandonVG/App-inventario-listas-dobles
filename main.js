63da143eaacb2b521135cf6f99c7b8837821f783
BrandonVG <54459465+BrandonVG@users.noreply.github.com>
2020-10-21 19:47:45 -0500

    Terminado

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-var productos = new Array(20);
-let contador = 0;
+var productos = [];
+var contador = 0;
 let cond = 0;
 let index = 0;
 var codigo = document.querySelector("#codigo");
@@ -7,6 +7,7 @@ var nombre = document.querySelector("#nombre");
 var desc = document.querySelector("#descripcion");
 var cantidad = document.querySelector("#cant");
 var costo = document.querySelector("#cost");
+var pos = document.querySelector("#pos");
 var btnAgregar = document.querySelector("#agregar");
 var btnBorrar = document.querySelector("#borrar");
 var btnBuscar = document.querySelector("#buscar");
@@ -14,43 +15,87 @@ var btnListar = document.querySelector("#listar");
 var btnListarI = document.querySelector("#listarInv");
 var div = document.querySelector("#result");
 btnAgregar.addEventListener("click",()=>{
-    cond = 0;
-    let validacion = validar();
-    if (validacion == 1){
-        var objeto = {
-            "id":codigo.value,
-            "nombre":nombre.value,
-            "desc":desc.value,
-            "cant":cantidad.value,
-            "cost":costo.value
-        }
-        if (contador < 20){
-            productos.forEach(p =>{
-                if (objeto.id == p.id){
-                    cond = 1;
+    if(pos.value == ""){
+        cond = 0;
+        let validacion = validar();
+        contador = productos.length;
+        if (validacion == 1){
+            var objeto = {
+                "id":codigo.value,
+                "nombre":nombre.value,
+                "desc":desc.value,
+                "cant":cantidad.value,
+                "cost":costo.value
+            }
+            if (contador < 20){
+                productos.forEach(p =>{
+                    if (objeto.id == p.id){
+                        cond = 1;
+                    }
+                })
+                if(cond == 0){
+                    productos[contador] = objeto;
+                    div.textContent="";
+                    div.insertAdjacentHTML("beforeend","<p>Producto agregado.</p>");
                 }
-            })
-            if(cond == 0){
-                productos[contador] = objeto;
-                contador++;
-                div.textContent="";
-                div.insertAdjacentHTML("beforeend","<p>Producto agregado.</p>");
+                else{
+                    div.textContent="";
+                    div.insertAdjacentHTML("beforeend","<p>Producto ya agregado.</p>");
+                }
             }
             else{
                 div.textContent="";
-                div.insertAdjacentHTML("beforeend","<p>Producto ya agregado.</p>");
+                div.insertAdjacentHTML("beforeend","<p>Máximo de productos</p>");
             }
         }
         else{
             div.textContent="";
-            div.insertAdjacentHTML("beforeend","<p>Máximo de productos</p>");
+            div.insertAdjacentHTML("beforeend","<p>Algún campo esta vacío.</p>");
         }
     }
     else{
-        div.textContent="";
-        div.insertAdjacentHTML("beforeend","<p>Algún campo esta vacío.</p>");
+        cond = 0;
+        let validacion = validar();
+        contador = productos.length;
+        if (validacion == 1 && (pos.value-1) < contador){
+            var objeto = {
+                "id":codigo.value,
+                "nombre":nombre.value,
+                "desc":desc.value,
+                "cant":cantidad.value,
+                "cost":costo.value
+            }
+            if (contador < 20){
+                productos.forEach(p =>{
+                    if (objeto.id == p.id){
+                        cond = 1;
+                    }
+                })
+                if(cond == 0){
+                    
+                    let a = pos.value - 1;
+                    for (let i = productos.length; i > a;i--){
+                        productos[i] = productos[i-1];
+                    }
+                    productos[a] = objeto;
+                    div.textContent="";
+                    div.insertAdjacentHTML("beforeend","<p>Producto agregado.</p>");
+                }
+                else{
+                    div.textContent="";
+                    div.insertAdjacentHTML("beforeend","<p>Producto ya agregado.</p>");
+                }
+            }
+            else{
+                div.textContent="";
+                div.insertAdjacentHTML("beforeend","<p>Máximo de productos</p>");
+            }
+        }
+        else{
+            div.textContent="";
+            div.insertAdjacentHTML("beforeend","<p>Algún campo esta vacío o no se puede insertar en el espacio indicado.</p>");
+        }
     }
-    
 })
 btnBorrar.addEventListener("click",()=>{
     index = encontrar(productos,codigo);
@@ -60,7 +105,7 @@ btnBorrar.addEventListener("click",()=>{
     }
     else{
         index--
-        delete productos[index];
+        borrar(productos,index);
         div.textContent="";
         div.insertAdjacentHTML("beforeend","<p>Producto eliminado.</p>");
     }
@@ -87,6 +132,7 @@ btnBuscar.addEventListener("click",()=>{
 })
 btnListar.addEventListener("click",()=>{
     crearTabla();
+    console.log(productos);
     let tabla = document.querySelector("#tabla");
     productos.forEach(p =>{
         let ren = tabla.insertRow(-1);
@@ -164,4 +210,12 @@ function validar(){
     else{
         return 1;
     }
+}
+function borrar(vector,id){
+    delete vector[id]
+    for(let j = id;j<vector.length;j++){
+        vector[j] = vector[j+1];
+    }
+    vector.length--;
+    return vector;
 }
